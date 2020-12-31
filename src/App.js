b3b81593f13fadab8041d08be75ce44d610a1370
Matthew Williams <matthew.rc3@gmail.com>
2020-12-30 20:46:51 -0500

    Started title page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 
 // Components
-import Title from './components/Title';
+import {Container} from '@material-ui/core'
+import Title from './components/Title'
 
 function App() {
 	return (
-    	<div className="main">
-			<Title />
-			<div className="content">
+    	<Container fixed>
+			<div className='title-main-container' >
+				<div className='title-name-container' >
+					<Title />
+				</div>
+				<div className='title-buttons-container'>
+					{[0,0,0,0].map(e => <div className='title-button'/>)}
+				</div>
 			</div>
-		</div>
+		</Container>
   	);
 }
 
--- src/components/Title.js
+++ src/components/Title.js
@@ -1,74 +1,43 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import '../css/title.css'
+const Title = props => {
+    const [clicked, setClicked] = useState(0);
+    const [keyframe, setKeyframe] = useState(0);
+    const lastClickedValue = 0;
 
-const Menu = props => {
-    // State
-    const [rotated, setRotated] = useState(false);
-    const [apart, setApart] = useState(false);
-    const [revealed, setRevealed] = useState(false);
-    // Keyframe function for burger menu
-    const onClick = () => {
-        var timer1, timer2;
-        if (rotated) {
-            setRevealed(!revealed)
-            timer1 = setTimeout(() => setRotated(!rotated), 400);
-            timer2 = setTimeout(() => setApart(!apart), 400);
-        } else {
-            setRotated(!rotated)
-            timer1 = setTimeout(() => setApart(!apart), 120);
-            timer2 = setTimeout(() => setRevealed(!revealed), 200);
+    // After the logo is clicked timers are started for each keyframe
+    useEffect(() => {
+        if (clicked === lastClickedValue) return
+        if (clicked) {
+            const timer1 = setTimeout(() => setKeyframe(1), 200);
+            const timer2 = setTimeout(() => setKeyframe(2), 300);
+            return () => clearTimeout(timer1, timer2);
         }
-        return () => clearTimeout(timer1, timer2);
-    }
-    // CSS 
-    return (
-        <div className='menu' 
-        style={{
-            width: revealed ? '275px' : '75px',
-            boxShadow: revealed ? "-4px 1px 24px 1px rgba(0,0,0,0.75)" : "-4px 1px 24px 1px rgba(0,0,0,0)",
-            background: revealed ? "rgba(255, 255, 255, 1)" : "rgba(255, 255, 255, 0)",
-        }}
-        >
-            {/** Burger Menu Animation*/ }
-            <div 
-            className='animation' 
-            style={{
-                transform: rotated ? 'rotate(0deg)' : 'rotate(-90deg)',
-                height: apart ? '160px' : '118px',
-            }}
-            onClick={() => onClick()}
-            >
-                <div className='animation-container' style={{top: '0px'}}>
-                    <div className='name'> M </div>
-                </div>
+        if (!clicked) {
+            const timer1 = setTimeout(() => setKeyframe(1), 200);
+            const timer2 = setTimeout(() => setKeyframe(0), 600);
+            return () => clearTimeout(timer1, timer2);
+        }
+        
+    }, [clicked])
 
-                <div className='animation-container' style={{bottom: '0px'}}>
-                    <div className='name'> W </div>
-                </div>
+    return (
+        <div 
+        className='name-animation' 
+        style={{
+            transform: keyframe < 2 ? 'rotate(0deg)' : 'rotate(-270deg)',
+            height: keyframe < 2 ? '190px' : '118px',
+            width: keyframe < 2 ? '' : '200px',
+        }}
+        onClick={() => setClicked(!clicked)}
+        >
+            <div className='name-container matthew' style={{width: keyframe < 1 ? '410px' : '82px'}}>
+                <div className='name'> Matthew </div>
             </div>
-            {/** Name Reveal*/ }
-            <div className='name-reveal' onClick={() => onClick()} >
-                <div className='name-container' style={{top: '0px', width: revealed ? '200px' : '0'}}>
-                    <div className='name blue' > atthew </div>
-                    <div className='name black'> atthew </div>
-                </div>
-                <div className='name-container' style={{bottom: '0px', width: revealed ? '200px' : '0'}}>
-                    <div className='name blue' > illiams </div>
-                    <div className='name black'> illiams </div>
-                </div>
-            </div>
-            {/** Menu Items*/ }
-            <div className='menu-items' style={{marginLeft: revealed ? '15px' : '-200px'}}>
-                <div className='menu-item' >
-                    About
-                </div>
-                <div className='menu-item' >
-                    Projects
-                </div>
-                <div className='menu-item' >
-                    Connect
-                </div>
+            <div className='name-container williams' style={{width: keyframe < 1 ? '410px' : '82px'}}>
+                <div className='name'> Williams </div>
             </div>
         </div>
     )
 }
-export default Menu;
+export default Title;
